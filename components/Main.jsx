8d67d26cea5ceb98284310d0cf4ce6e3ffec9327
mabrursatori <mabrursatori@gmail.com>
2022-06-20 12:56:34 +0700

    Ad Main

---
+++ components/Main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+export default function Main() {
+  return (
+    <div className="w-full h-screen text-center">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+            <div>
+                <p className="uppercase text-sm tracking-widest text-gray-600">LETS BUILD SOMETHING TOGETHER</p>
+                <h1 className='py-4 text-gray-700'>Hi, Im <span className='text-[#5651e5]'>Mabrur</span></h1>
+                <h1 className='py-2 text-gray-700'>
+                   A Front-End Web Developer
+                </h1>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                    Im a front-end developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive front-end web applications while learnig back-end technologies.
+                </p>
+                <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <FaLinkedin/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <FaGithub/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <AiOutlineMail/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <BsFillPersonLinesFill/>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
