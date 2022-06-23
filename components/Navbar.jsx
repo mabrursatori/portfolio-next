4e76b9204e0008e22cea29dc42e65846b766bd7b
mabrursatori <mabrursatori@gmail.com>
2022-06-23 10:52:34 +0700

    finish

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,15 +4,33 @@ import Link from 'next/link'
 import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai';
 import {FaLinkedin, FaGithub} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { useRouter } from 'next/router';
 
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const [shadow, setShadow] = useState(false);
-
+    const [navBg, setNavBg] = useState('#ecf0f3');
+    const [linkColor, setLinkColor] = useState('#1f2937');
+    const router = useRouter();
     const handleNav = () => {
         setNav(!nav);
     }
+
+    useEffect(() => {
+    if (
+      router.asPath === '/property' ||
+      router.asPath === '/crypto' ||
+      router.asPath === '/netflix' ||
+      router.asPath === '/twitch'
+    ) {
+      setNavBg('transparent');
+      setLinkColor('#ecf0f3');
+    } else {
+      setNavBg('#ecf0f3');
+      setLinkColor('#1f2937');
+    }
+    }, [router]);
 
     useEffect(() => {
         const handleShadow = () => {
@@ -27,13 +45,15 @@ const Navbar = () => {
     }, []);
 
     return (
-        <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+        <div
+        style={{ backgroundColor: `${navBg}` }}
+        className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/'>
                 <Image src="/../public/assets/navLogo.png" alt="/" width="125" height="50" />
                 </Link>
                 <div>
-                    <ul className="hidden md:flex">
+                    <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
                         <Link href="/">
                             <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
                         </Link>
@@ -74,19 +94,19 @@ const Navbar = () => {
                     <div className='py-4 flex flex-col'>
                         <ul className='uppercase'>
                             <Link href='/'>
-                            <li className='py-4 text-sm'>Home</li>
+                            <li onClick={handleNav} className='py-4 text-sm'>Home</li>
                             </Link>
-                            <Link href='/'>
-                            <li className='py-4 text-sm'>About</li>
+                            <Link href='/#about'>
+                            <li onClick={handleNav} className='py-4 text-sm'>About</li>
                             </Link>
-                            <Link href='/'>
-                            <li className='py-4 text-sm'>Skills</li>
+                            <Link href='/#skills'>
+                            <li onClick={handleNav} className='py-4 text-sm'>Skills</li>
                             </Link>
-                            <Link href='/'>
-                            <li className='py-4 text-sm'>Projects</li>
+                            <Link href='/#projects'>
+                            <li onClick={handleNav} className='py-4 text-sm'>Projects</li>
                             </Link>
-                            <Link href='/'>
-                            <li className='py-4 text-sm'>Contact</li>
+                            <Link href='/#contact'>
+                            <li onClick={handleNav} className='py-4 text-sm'>Contact</li>
                             </Link>
                         </ul>
                         <div className='pt-40'>
