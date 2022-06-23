8ee545f03e4fe14e271a96b85828f74e04824862
mabrursatori <mabrursatori@gmail.com>
2022-06-23 13:03:46 +0700

    'fixing

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,23 +11,33 @@ export default function Main() {
                 <p className="uppercase text-sm tracking-widest text-gray-600">LET&#39;S BUILD SOMETHING TOGETHER</p>
                 <h1 className='py-4 text-gray-700'>Hi, I&#39;m <span className='text-[#5651e5]'>Mabrur</span></h1>
                 <h1 className='py-2 text-gray-700'>
-                   A Front-End Web Developer
+                   A Junior Software Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                    I&#39;m a front-end developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I&#39;m focused on building responsive front-end web applications while learnig back-end technologies.
+                  I&#39;m Junior Software Developer, I&#39;m exciting, commitment and professional for my job and my projects. Let&#39;s scroll down and know more about me.
                 </p>
                 <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
                 <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <FaLinkedin/>
+                  <a href='https://www.linkedin.com/in/mabrur-371b44195/' target="_blank" rel='noreferrer'>
+                  <FaLinkedin/>
+                  </a>
+                                   
                 </div>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href="https://github.com/mabrursatori" target="_blank" rel='noreferrer'>
                                     <FaGithub/>
+                </a>
                 </div>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <AiOutlineMail/>
+                
+                    <AiOutlineMail/>
+                
+                                   
                 </div>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href="https://api.whatsapp.com/send?phone=6285624436317" target="_blank" rel='noreferrer'>
                                     <BsFillPersonLinesFill/>
+                  </a>
                 </div>
                 </div>
             </div>
