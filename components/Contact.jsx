59ab6db46b171662fcc7affa88c7bd5a964e6999
mabrursatori <mabrursatori@gmail.com>
2022-06-24 15:06:01 +0700

    Add email.js and toast custom

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,26 +1,45 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import ContactImg from '../public/assets/contact.jpg'
+import emailjs from '@emailjs/browser';
+import { MdClose } from "react-icons/md";
+import { AiFillSmile } from "react-icons/ai";
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
+    const [showToast, setShowToast] = useState(false);
+    const form = useRef();
 
-    const handleSubmit = () => {
-        setName('');
-        setPhone('');
-        setEmail('');
-        setSubject('');
-        setMessage('');
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(
+            'service_2v7rryh',
+            'template_ktk11ri',
+            form.current,
+            "t0jcbMmalB_GQSrOu"
+        )
+                    .then(function() {
+                        console.log('SUCCESS!');
+                        setShowToast(true);
+                        setTimeout(() => {
+                            setShowToast(false);
+                        }, 1000);
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
     };
+
+    const closeToast = () => {
+        setShowToast(false);
+    }
+
+
+    
+    
 
   return (
     <div id='contact' className='w-full lg:h-screen'>
@@ -76,39 +95,51 @@ const Contact = () => {
                 {/* rigth */}
                 <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                     <div className='p-4'>
-                        <form>
+                        <form ref={form} onSubmit={sendEmail}>
                             <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                                 <div className='flex flex-col'>
                                     <label className='uppercase text-sm py-2'>Name</label>
                                     <input className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='text'
+                                    type='text' name='name'
                                     />
                                 </div>
                                 <div className='flex flex-col'>
                                     <label className='uppercase text-sm py-2'>Phone Number</label>
                                     <input className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='text'
+                                    type='text' name='phone'
                                     />
                                 </div>
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Email</label>
-                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email'/>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                type='email'
+                                name='email'
+                                />
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Subject</label>
-                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                name='subject'
+                                type='text'/>
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Message</label>
-                                <textarea className='border-2 rounded-lg p-3' rows='10'></textarea>
+                                <textarea className='border-2 rounded-lg p-3' rows='10' name='message'></textarea>
                             </div>
-                            <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+                            <button type='submit' className='w-full p-4 bg-[#5651e5] text-gray-100 mt-4'>Send Message</button>
                         </form>
                     </div>
                 </div>
                
             </div>
+                <div
+                className={`flex justify-evenly items-center fixed  h-[70px] w-[250px] bg-green-200  right-[50px] rounded-lg shadow-lg shadow-gray-500 ${showToast ? 'opacity-100 top-[100px]' : 'opacity-0 top-[50px]'} ease-in duration-100`}
+                >
+                    <p>Your message sent <AiFillSmile className='inline text-yellow-500 text-[25px]'/>
+                    <br/> Please wait for my reply </p>
+                    <MdClose onClick={closeToast} className='text-[30px] cursor-pointer'/>
+                </div>
             <div className='flex justify-center py-12'>
                     <Link href='/'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
