95474dbac98994ee1f59670610651dc5bef27bfd
mabrursatori <mabrursatori@gmail.com>
2022-07-18 01:23:06 +0700

    fixing api9

--- components/Main.jsx
+++ components/Main.jsx
@@ -12,7 +12,7 @@ export default function Main() {
                 <p className="uppercase text-sm tracking-widest text-gray-600">LET&#39;S BUILD SOMETHING TOGETHER</p>
                 <h1 className='py-4 text-gray-700'>Hi, I&#39;m <span className='text-[#5651e5]'>Mabrur</span></h1>
                 <h1 className='py-2 text-gray-700'>
-                   A Junior Software Developer
+                   A Junior Full-Stack Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
                   I&#39;m Full-Stack Developer, I&#39;m exciting, commitment and professional for my job and my projects. Let&#39;s scroll down and know more about me.
